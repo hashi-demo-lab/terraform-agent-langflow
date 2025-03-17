cc04fc8fdaaf433c78630c41525ce730c7913617
Simon Lynch <simon.lynch@hashicorp.com>
2025-03-18 01:13:16 +1100

    feat: implement AWS S3 bucket resource and update development agent prompt

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  acl    = var.acl
+
+  versioning {
+    enabled = var.versioning
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = var.sse_algorithm
+      }
+    }
+  }
+}
